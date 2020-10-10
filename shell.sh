2eee06326070570bb1ab30874e747b34859360c0
L99002516 <71586696+L99002516@users.noreply.github.com>
2020-10-10 16:43:04 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,7 +1,5 @@
 gcc server.c -o Server -lpthread
 gcc client.c -o Client -lpthread
-gnome-terminal -e ./Client
-gnome-terminal -e ./Client
 cppcheck --enable=all ./Server 2> servercppcheck.txt
 cppcheck --enable=all ./Client 2> clientcppcheck.txt
 valgrind ./Server 2> servervalgrind.txt
